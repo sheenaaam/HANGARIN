80b10ea1a909554c28eda4bee43e418796afc1c8
sheenaaam <sheenamrivera15@gmail.com>
2026-03-23 16:50:33 +0000

    updated

--- hangarin/settings.py
+++ hangarin/settings.py
@@ -1,8 +1,3 @@
-
-
-
-
-
 """
 Django settings for hangarin project.
 
@@ -16,7 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+
+import socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-65$+tfy6#o^1!es5^szcf)ut3bnfc25%=sa%r9&zz@td#!-)_m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['shx15.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','shx15.pythonanywhere.com']
 
 
 # Application definition
@@ -43,7 +39,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
     'tasks',
+    'widget_tweaks',
+]
+
+SITE_ID = 3
+if "pythonanywhere" in socket.gethostname():
+    SITE_ID = 2
+else :
+    SITE_ID = 4
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +66,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -61,7 +76,7 @@ ROOT_URLCONF = 'hangarin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,13 +136,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
- HEAD
 
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'
- 78c3ef8 (changes from pythonanywhere)
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+
+ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login'
+ACCOUNT_LOGOUT_ON_GET = True
+
+ACCOUNT_LOGIN_METHODS = {"username", "email"}
+ACCOUNT_SIGNUP_FIELDS = [
+    "username*",
+    "email*",
+    "password1*",
+    "password2*",
+]
--- hangarin/urls.py
+++ hangarin/urls.py
@@ -14,14 +14,64 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
+URL configuration for hangarin project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
 from django.contrib import admin
 from django.urls import path, include
+from tasks.views import (
+    DashboardView,
+    TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView,
+    SubTaskListView, SubTaskCreateView, SubTaskUpdateView, SubTaskDeleteView,
+    CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView,
+    PriorityListView, PriorityCreateView, PriorityUpdateView, PriorityDeleteView,
+    NoteListView, NoteCreateView, NoteUpdateView, NoteDeleteView
+)
+
 
 urlpatterns = [
-
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
 
-    path("", include("tasks.urls")),
 
+    path("", DashboardView.as_view(), name="dashboard"),
+
+    path("tasks/", TaskListView.as_view(), name="task_list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task_create"),
+    path("tasks/update/<int:pk>/", TaskUpdateView.as_view(), name="task_update"),
+    path("tasks/delete/<int:pk>/", TaskDeleteView.as_view(), name="task_delete"),
+
+    path("subtasks/", SubTaskListView.as_view(), name="subtask_list"),
+    path("subtasks/create/", SubTaskCreateView.as_view(), name="subtask_create"),
+    path("subtasks/update/<int:pk>/", SubTaskUpdateView.as_view(), name="subtask_update"),
+    path("subtasks/delete/<int:pk>/", SubTaskDeleteView.as_view(), name="subtask_delete"),
+
+    path("categories/", CategoryListView.as_view(), name="category_list"),
+    path("categories/create/", CategoryCreateView.as_view(), name="category_create"),
+    path("categories/update/<int:pk>/", CategoryUpdateView.as_view(), name="category_update"),
+    path("categories/delete/<int:pk>/", CategoryDeleteView.as_view(), name="category_delete"),
+
+    path("priorities/", PriorityListView.as_view(), name="priority_list"),
+    path("priorities/create/", PriorityCreateView.as_view(), name="priority_create"),
+    path("priorities/update/<int:pk>/", PriorityUpdateView.as_view(), name="priority_update"),
+    path("priorities/delete/<int:pk>/", PriorityDeleteView.as_view(), name="priority_delete"),
+
+    path("notes/", NoteListView.as_view(), name="note_list"),
+    path("notes/create/", NoteCreateView.as_view(), name="note_create"),
+    path("notes/update/<int:pk>/", NoteUpdateView.as_view(), name="note_update"),
+    path("notes/delete/<int:pk>/", NoteDeleteView.as_view(), name="note_delete"),
 ]
-
