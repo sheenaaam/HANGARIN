abfc8e79c03eb52b2388cddc596545ba99aa60d1
sheenaaam <sheenamrivera15@gmail.com>
2026-04-07 05:46:47 +0000

    modify

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
 from django.urls import reverse_lazy
 from .models import Task, SubTask, Category, Priority, Note
